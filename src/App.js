7c2e4b9b5c2aea26b4cb5b2ece288018ed2fbf50
Hezaa123 <69940637+Hezaa123@users.noreply.github.com>
2020-12-01 21:01:54 +0000

    Added a new component "Weather" for the Weather Application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
+      </Weather>
       <div className="Footer">
             <small>
                 <a href="https://github.com/Hezaa123/weather-app-react" target="_blank" rel="noreferrer">Open-source code </a>
                 by 
                 <a href="https://www.linkedin.com/in/heather-kirwan-581a4580/" target="_blank" rel="noreferrer"> Heather Kirwan</a>
             </small>
-        </div>
+      </div>
     </div>
   );
 }
