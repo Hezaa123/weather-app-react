2bbf7443f9ae9a62317113ac4c7dd6473d5dc17e
Hezaa123 <69940637+Hezaa123@users.noreply.github.com>
2020-12-01 22:07:32 +0000

    Installed fontawesome and created a search location form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      </Weather>
+      <Weather/>
       <div className="Footer">
-            <small>
-                <a href="https://github.com/Hezaa123/weather-app-react" target="_blank" rel="noreferrer">Open-source code </a>
-                by 
-                <a href="https://www.linkedin.com/in/heather-kirwan-581a4580/" target="_blank" rel="noreferrer"> Heather Kirwan</a>
-            </small>
+        <small>
+          <a href="https://github.com/Hezaa123/weather-app-react" target="_blank" rel="noreferrer">Open-source code </a>
+          by 
+          <a href="https://www.linkedin.com/in/heather-kirwan-581a4580/" target="_blank" rel="noreferrer"> Heather Kirwan</a>
+        </small>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import "./Weather.css";
 
 export default function Weather(){
-    return();
+    let searchForm = (
+        <div className="Weather searchForm">
+            <form>
+                <input
+                    className="Weather searchBar"
+                    type="search"
+                    placeholder="Search location.."
+                    autoFocus={false}
+                />
+                <button className="Weather searchButton" type="submit">
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
+                <button className="Weather geoLocationButton">
+                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                </button>
+            </form>
+        </div>
+    )
+    return(
+        <div className="Weather">
+            {searchForm}
+        </div>
+    );
 }
