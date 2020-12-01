40a42378d285c9758fbd70a44229cd17c41fa156
Hezaa123 <69940637+Hezaa123@users.noreply.github.com>
2020-12-01 20:55:14 +0000

    Added a footer that links to Git Repository and LinkedIn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      
+      <div className="Footer">
+            <small>
+                <a href="https://github.com/Hezaa123/weather-app-react" target="_blank" rel="noreferrer">Open-source code </a>
+                by 
+                <a href="https://www.linkedin.com/in/heather-kirwan-581a4580/" target="_blank" rel="noreferrer"> Heather Kirwan</a>
+            </small>
+        </div>
     </div>
   );
 }
